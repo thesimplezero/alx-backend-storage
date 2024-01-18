6e712c97f6a743a152b28f73347389b319562291
thesimplezero <hi@thesimplezero.io>
2024-01-18 19:11:07 +0000

    List Glam rock bands ranked by longevity

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- List Glam rock bands ranked by longevity.
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
+
